1e677c69f2164cbfbb69d5d970184a6e7b8b49d1
Marce Viera <marce.16.viera@gmail.com>
2025-05-27 02:33:51 -0300

    update 2

--- presentacion_personal.py
+++ presentacion_personal.py
@@ -1,3 +1,2 @@
-#archivo
-print("Tengo conocimientos en SQL server, python, LINUX y he realizado testing de software " \
-"actualmente realizo soporte tecnico de un software para heladerias.")
+#Tengo conocimientos en SQL server, python, LINUX y he realizado testing de software
+#actualmente realizo soporte tecnico de un software para heladerias.
